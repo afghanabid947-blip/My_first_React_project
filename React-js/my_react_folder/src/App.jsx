7ad3e42a6afa87babce325ed1b9624960ddfcdd8
Afghan Abid <afghanabid947@gmail.com>
2026-03-26 13:40:01 +0500

    initial commit

---
+++ React-js/my_react_folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>my name is khan</h1>
+      <h2>my name is asif</h2>
+    </div>
+  );
+};
+
+export default App;
